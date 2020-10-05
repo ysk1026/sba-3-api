18e62698095b2241edc85a9883ce5c943520babc
Kim <fkqoseka@gmail.com>
2020-10-05 12:22:43 +0900

    12:20

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def idx():
-    return 'Hello world'
+class Rest(Resource):
+    def get(self):
+        return {'rest' : 'Good !'}
+
+api.add_resource(Rest, '/')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host = '127.0.0.1', port = 8080)
      
