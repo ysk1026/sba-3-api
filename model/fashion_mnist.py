ae497fb0c2949894937f202d6d2220495e14574f
Kim <fkqoseka@gmail.com>
2020-10-07 17:16:36 +0900

    17:16

--- model/fashion_mnist.py
+++ model/fashion_mnist.py
@@ -3,7 +3,7 @@ import os
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 
 
@@ -13,11 +13,13 @@ class FashionMnist:
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
     def hook(self):
-        self.create_model()
         image_list = self.get_data()
         self.preprocess(image_list[0], image_list[1])
-
-
+        model = self.create_model()
+        model = self.train_model(model, image_list[0], image_list[1])
+        # self.test_model(model, image_list[2], image_list[3])
+        self.model_predict(model, image_list[2], image_list[3], 0)
+        
     def get_data(self) -> []:
         (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
 
@@ -43,12 +45,72 @@ class FashionMnist:
             plt.yticks([])
             plt.grid(False)
             plt.imshow(train_images[i], cmap=plt.cm.binary)
-            plt.xlabel(class_names[train_labels[i]])
-        plt.show()
+            plt.xlabel(self.class_names[train_labels[i]])
+        # plt.show()
+    
+    def create_model(self) -> object:
+        model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(128, activation='relu'),
+        keras.layers.Dense(10, activation='softmax')
+        ])
         
+        model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
+        return model
 
+    def train_model(self, model, train_images, train_labels):
+        model.fit(train_images, train_labels, epochs=5)
+        return model
+    
+    
+    def test_model(self, model, test_images, test_labels):
+        test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+        print('\n테스트 정확도:', test_acc)
+    
+    def model_predict(self, model, test_images, test_labels, index):
+        predictions = model.predict(test_images)
+        pred = predictions[index]
+        answer = test_labels[index]
+        print(f'모델이 예측한 값: {np.argmax(pred)}')
+        print(f'정답 : {answer}')
+        return [predictions, test_images, test_labels]
+    
+    def plot_image(self, i, predictions_array, true_label, img) -> []:
+        predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+        plt.grid(False)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(img, cmap=plt.cm.binary)
+        
+        prediction_label = np.argmax(predictions_array)
+        if prediction_label == true_label:
+            color = 'blue'
+        else:
+            color = 'red'
+        plt.xlabel(f' {} : {} %'.format(self.class_names[prediction_label],
+                   100 * np.max(predictions_array),
+                   self.class_names[true_label], color
+                   ))
+        
+    @staticmethod
+    def plot_value_array(i, predictions_array, true_label):
+        predictions_array, true_label = predictions_array[i], true_label[i]
+        plt.grid(False)
+        plt.xticks([])
+        plt.yticks([])
+        thisplot = plt.bar(range(10), predictions_array, color = '#777777')
+        plt.ylim([0, 1])
+        prediction_label = np.argmax(predictions_array)
+        thisplot[prediction_label].set_color('red')
+        thisplot[true_label].set_color('blue') 
+        
+        
+        
 if __name__ == '__main__':
     fashion = FashionMnist()
-    fashion.get_data()
+    fashion.hook()
     
