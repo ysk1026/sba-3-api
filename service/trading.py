2bb1b2faf04acbb506d03a54fbf35d27a5ba653d
Kim <fkqoseka@gmail.com>
2020-10-08 17:24:45 +0900

    17:20

--- service/trading.py
+++ service/trading.py
@@ -1,66 +1,34 @@
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-baseurl = os.path.dirname(os.path.abspath(__file__))
 import numpy as np
-import tensorflow as tf 
-from tensorflow import keras
-from collections import deque
-import random
+import math
+import pandas_datareader as data_reader
 
-class AiTrader:
-    
-    model : object = None
-    train_images : object = None
-    train_labels : object = None
-    test_images : object = None
-    test_lables : object = None
-    
-    def __init__(self, state_size, action_space=3, model_name='AITrader'):
-        self.state_size = state_size
-        self.action_space = action_space
-        self.memory = deque(maxlen=2000)
-        self.inventory = []
-        self.model_name = model_name
-        self.gamma = 0.95
-        self.epsilon = 1.0
-        self.epsilon_final = 0.01
-        self.epsilon_decay = 0.995
-    
-    def model_builder(self):
-        model = tf.keras.models.Sequential()
-        model.add(tf.keras.layers.Dense(units=32, activation='relu', input_dim=self.state_size))
-        model.add(tf.keras.layers.Dense(units=64, activation='relu'))
-        model.add(tf.keras.layers.Dense(units=128, activation='relu'))
-        model.add(tf.keras.layers.Dense(units=self.action_space, activation='linear'))
-        model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(lr=0.001))
-        
-        self.model = model
-        
-    def batch_model(self, batch_size):
-        batch = []
-        for i in range(len(self.memory) - batch_size + 1, len(self.memory)):
-            batch.append(self.memory[i])
-        for state, action, reward, next_state, done in batch:
-            reward = reward
-            if not done:
-                reward = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
-            target = self.model.predict(state)
-            target[0][action] = reward
-            self.model.fit(state, target, epochs=1, verbose=0)
-        if self.epsilon > self.epsilon_final:
-            self.epsilon *= self.epsilon_decay
-            
-    def trade(self, state):
-        if random.random() <= self.epsilon:
-            return random.randrange(self.action_space)
-        actions = self.model.predict(state)
-        return np.argmax(actions[0])
-        
-    
-if __name__ == '__main__':
-    api = AiTrader()
-    api.hook()
+class Trading:
+    @staticmethod
+    def sigmoid(x):
+        return 1 / (1 + math.exp(-x))
+    @staticmethod
+    def stocks_price_format(n):
+        if n < 0:
+            return "- $ {0:2f}".format(abs(n))
+        else:
+            return "$ {0:2f}".format(abs(n))
+    @staticmethod
+    def dataset_loader(stock_name):
+        dataset = data_reader.DataReader(stock_name, data_source="yahoo")
+        start_date = str(dataset.index[0]).split()[0]
+        end_date = str(dataset.index[-1]).split()[0]
+        close = dataset['Close']
+        return close
+    def state_creator(self,data, timestep, window_size):
+        starting_id = timestep - window_size + 1
+        if starting_id >= 0:
+            windowed_data = data[starting_id: timestep + 1]
+        else:
+            windowed_data =- starting_id * [data[0]] + list(data[0:timestep + 1])
+        state = []
+        for i in range(window_size - 1):
+            state.append(self.sigmoid(windowed_data[i + 1] - windowed_data[i]))
+        return np.array([state])
         
         
     
