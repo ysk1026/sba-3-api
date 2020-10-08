c6394317ef74a2b102996b76a624f6262b66854a
Kim <fkqoseka@gmail.com>
2020-10-08 17:40:27 +0900

    17:40

--- model/ai_trader.py
+++ model/ai_trader.py
@@ -1,21 +1,10 @@
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-baseurl = os.path.dirname(os.path.abspath(__file__))
 import numpy as np
 import tensorflow as tf 
-from tensorflow import keras
 from collections import deque
 import random
 
 class AiTrader:
-    
-    model : object = None
-    train_images : object = None
-    train_labels : object = None
-    test_images : object = None
-    test_lables : object = None
-    
+
     def __init__(self, state_size, action_space=3, model_name='AITrader'):
         self.state_size = state_size
         self.action_space = action_space
@@ -26,7 +15,8 @@ class AiTrader:
         self.epsilon = 1.0
         self.epsilon_final = 0.01
         self.epsilon_decay = 0.995
-    
+        self.model = self.model_builder()
+
     def model_builder(self):
         model = tf.keras.models.Sequential()
         model.add(tf.keras.layers.Dense(units=32, activation='relu', input_dim=self.state_size))
@@ -34,10 +24,9 @@ class AiTrader:
         model.add(tf.keras.layers.Dense(units=128, activation='relu'))
         model.add(tf.keras.layers.Dense(units=self.action_space, activation='linear'))
         model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(lr=0.001))
+        return model
         
-        self.model = model
-        
-    def batch_model(self, batch_size):
+    def batch_train(self, batch_size):
         batch = []
         for i in range(len(self.memory) - batch_size + 1, len(self.memory)):
             batch.append(self.memory[i])
@@ -50,14 +39,11 @@ class AiTrader:
             self.model.fit(state, target, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_final:
             self.epsilon *= self.epsilon_decay
-            
+
     def trade(self, state):
         if random.random() <= self.epsilon:
             return random.randrange(self.action_space)
         actions = self.model.predict(state)
         return np.argmax(actions[0])
-        
+
     
-if __name__ == '__main__':
-    api = AiTrader()
-    api.hook()
--- service/trading.py
+++ service/trading.py
@@ -1,11 +1,19 @@
-import numpy as np
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+baseurl = os.path.dirname(os.path.abspath(__file__))
 import math
 import pandas_datareader as data_reader
+import numpy as np
+from tqdm import tqdm
+from model.ai_trader import AiTrader
 
+# pip install pandas-datareader
 class Trading:
     @staticmethod
     def sigmoid(x):
         return 1 / (1 + math.exp(-x))
+    
     @staticmethod
     def stocks_price_format(n):
         if n < 0:
@@ -29,7 +37,53 @@ class Trading:
         for i in range(window_size - 1):
             state.append(self.sigmoid(windowed_data[i + 1] - windowed_data[i]))
         return np.array([state])
-        
-        
-    
-        
+
+    """
+    hook method
+    """
+    def transaction(self, target):
+        stock_name = target
+        data = self.dataset_loader(stock_name)
+        window_size = 10
+        episodes = 1000
+        batch_size = 32
+        data_samples = len(data) - 1
+        trader = AiTrader(window_size)
+        print('==== Model Summary ===')
+        print(trader.model.summary())
+        for episode in range(1, episodes + 1):
+            print("Episode: {}/{}".format(episode, episodes))
+            state = self.state_creator(data, 0, window_size + 1)
+            total_profit = 0
+            trader.inventory = []
+            for t in tqdm(range(data_samples)):
+                action = trader.trade(state)
+                next_state = self.state_creator(data, t + 1, window_size + 1)
+                reward = 0
+                if action == 1: # Buying
+                    trader.inventory.append(data[t])
+                    print("AI 트레이더 매수: ", self.stocks_price_format(data[t]))
+                elif action == 2 and len(trader.inventory) > 0: # Selling
+                    buy_price = trader.inventory.pop(0)
+                    reward = max(data[t] - buy_price, 0)
+                    total_profit += data[t] - buy_price
+                    print("AI 트레이더 매도: ", self.stocks_price_format(data[t]),
+                          "이익: "+self.stocks_price_format(data[t] - buy_price))
+                if t == data_samples - 1:
+                    done = True
+                else:
+                    done = False
+                trader.memory.append((state, action, reward, next_state, done))
+                state = next_state
+                if done:
+                    print('#################')
+                    print('총이익: {}'.format(total_profit))
+                    print('#################')
+                if len(trader.memory) > batch_size:
+                    trader.batch_train(batch_size)
+            if episode % 10 == 0:
+                trader.model.save('ai_trader_{}.h5'.format(episode))
+
+if __name__ == '__main__':
+    trading = Trading()
+    trading.transaction('AAPL')
